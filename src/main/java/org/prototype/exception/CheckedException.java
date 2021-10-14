f8347169f7b0587d1fb2fc76f6473d2f2de23683
arkaprovo <apb@live.in>
2021-10-14 08:40:20 +0530

    feat(light-cd): user defined exception class added

---
+++ src/main/java/org/prototype/exception/CheckedException.java
@@ -0,0 +1,15 @@
+package org.prototype.exception;
+
+public class CheckedException extends RuntimeException {
+    public CheckedException(String message) {
+        super(message);
+    }
+
+    public CheckedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CheckedException(Throwable cause) {
+        super(cause);
+    }
+}
