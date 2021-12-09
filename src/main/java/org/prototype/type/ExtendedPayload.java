90fd9df658eef14ff81d0df3b36aa30d178b15a1
arkaprovo <apb@live.in>
2021-12-09 09:07:14 +0530

    refactor: made changes in local build script, added label shard in ingress controllers

---
+++ src/main/java/org/prototype/type/ExtendedPayload.java
@@ -0,0 +1,32 @@
+package org.prototype.type;
+
+import java.util.Map;
+
+public class ExtendedPayload {
+    Payload payload;
+    Map<String,String> templateParameters;
+
+    public Payload getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Payload payload) {
+        this.payload = payload;
+    }
+
+    public Map<String, String> getTemplateParameters() {
+        return templateParameters;
+    }
+
+    public void setTemplateParameters(Map<String, String> templateParameters) {
+        this.templateParameters = templateParameters;
+    }
+
+    @Override
+    public String toString() {
+        return "{"
+                + "\"payload\":" + payload
+                + ", \"templateParameters\":" + templateParameters
+                + "}";
+    }
+}
