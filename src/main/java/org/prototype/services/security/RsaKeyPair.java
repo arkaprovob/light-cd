d44681d3b513c52500563acbc860897fd9819760
arkaprovo <apb@live.in>
2022-04-25 20:22:44 +0530

    added support for generate RSA keys from properties, and fixed SPASHIP_AUTH__KEYCLOAK_SECRET issue.

--- src/main/java/org/prototype/services/security/RsaKeyPair.java
+++ src/main/java/org/prototype/services/security/RsaKeyPair.java
@@ -7,7 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.security.*;
-import java.util.Base64;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.*;
 
 
 public class RsaKeyPair {
@@ -27,6 +30,15 @@ public class RsaKeyPair {
     @SneakyThrows
     void onStart() {
 
+        Map<String,Key> keyPair = generateKeyPairFromProperties();
+
+        if(!keyPair.isEmpty()){
+            LOG.info("key value pair found in configuration!");
+            this.privateKey = (PrivateKey) keyPair.get("private");
+            this.publicKey = (PublicKey) keyPair.get("public");
+            return;
+        }
+
         KeyPairGenerator keyPairGenerator =
                 java.security.KeyPairGenerator.getInstance("RSA");
         SecureRandom secureRandom = new SecureRandom();
@@ -40,6 +52,21 @@ public class RsaKeyPair {
         LOG.info("RsaKeyPair generated");
     }
 
+    private Map<String,Key> generateKeyPairFromProperties() throws NoSuchAlgorithmException,
+            InvalidKeySpecException {
+
+        String encodedPublicKey = ConfigProvider.getConfig()
+                .getValue("rsa.public.key", String.class);
+        String encodedPrivateKey = ConfigProvider.getConfig()
+                .getValue("rsa.private.key", String.class);
+
+        if(Objects.isNull(encodedPrivateKey) || Objects.isNull(encodedPublicKey) ||
+                encodedPrivateKey.isEmpty() || encodedPublicKey.isBlank())
+            return Map.of();
+
+        return Map.of("public",publicKeyFromString(encodedPublicKey),
+                "private",privateKeyFromString(encodedPrivateKey));
+    }
 
     public PrivateKey getPrivateKey() {
         return privateKey;
@@ -65,6 +92,20 @@ public class RsaKeyPair {
         return Base64.getEncoder().encodeToString(privateKey.getEncoded());
     }
 
+
+    private PublicKey publicKeyFromString(String encodedPublicKey) throws NoSuchAlgorithmException,
+            InvalidKeySpecException {
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec pks = new X509EncodedKeySpec(Base64.getDecoder().decode(encodedPublicKey));
+        return kf.generatePublic(pks);
+    }
+
+    private PrivateKey privateKeyFromString(String encodedPrivateKey) throws NoSuchAlgorithmException,
+            InvalidKeySpecException {
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec pks = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(encodedPrivateKey));
+        return kf.generatePrivate(pks);
+    }
 
 
 
